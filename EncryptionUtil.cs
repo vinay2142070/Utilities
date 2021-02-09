f972dec84242f301ba3fc1aa69892ce797726b01
vinay2142070 <50856679+vinay2142070@users.noreply.github.com>
2021-02-09 19:45:49 +0530

    C# code for encrypt_decrypt using AES and RSA

---
+++ EncryptionUtil.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Aes_Example
+{
+    class EncryptionUtil
+    {
+       public static byte[] AESEncrypt(string text, byte[] key, byte[] IV)
+        {
+            byte[] encrypted;
+            using (AesManaged aes = new AesManaged())
+            {
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+                // aes.Padding = PaddingMode.None;
+                aes.BlockSize = 128;
+                aes.KeySize = 128;
+
+                ICryptoTransform encryptor = aes.CreateEncryptor(key, IV);
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter sw = new StreamWriter(cs))
+                            sw.Write(text);
+                        encrypted = ms.ToArray();
+                    }
+                }
+            }
+            return encrypted;
+        }
+
+       public static string AESDecrypt(string hexText, byte[] key, byte[] IV)
+        {
+            byte[] text = HexadecimalStringToByteArray(hexText);
+            string plainText = null;
+            using (AesManaged aes = new AesManaged())
+            {
+                aes.Mode = CipherMode.CBC;
+                aes.Padding = PaddingMode.PKCS7;
+                // aes.Padding = PaddingMode.None;
+                aes.BlockSize = 128;
+                aes.KeySize = 128;
+
+                ICryptoTransform decryptor = aes.CreateDecryptor(key, IV);
+                using (MemoryStream ms = new MemoryStream(text))
+                {
+                    using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader reader = new StreamReader(cs))
+                        {
+                            plainText = reader.ReadToEnd();
+                        }
+                    }
+                }
+            }
+            return plainText;
+        }
+
+        public static byte[] HexadecimalStringToByteArray(string input)
+        {
+            var outputLength = input.Length / 2;
+            var output = new byte[outputLength];
+            using (var sr = new StringReader(input))
+            {
+                for (var i = 0; i < outputLength; i++)
+                    output[i] = Convert.ToByte(new string(new char[2] { (char)sr.Read(), (char)sr.Read() }), 16);
+            }
+            return output;
+        }
+
+        public static byte[] copyOfRange(byte[] original, int from, int to)
+        {
+            int newLength = to - from;
+            byte[] copy = new byte[newLength];
+            Array.Copy(original, from, copy, 0,
+                             Math.Min(original.Length - from, newLength));
+           return copy;
+        }
+
+        public static string RSAPublicKeyEncrypt(string publickey, string aesHexKey, string partnerID)
+        {
+
+             string tobeEncryptedString = partnerID + ";;" + aesHexKey;
+            var csp = new RSACryptoServiceProvider(2048);
+            csp.FromXmlString(publickey);
+
+            //for encryption, always handle bytes...
+            var bytesPlainTextData = Encoding.UTF8.GetBytes(tobeEncryptedString);
+
+            //apply pkcs#1.5 padding and encrypt our data 
+            var bytesEncryptedText = csp.Encrypt(bytesPlainTextData, RSAEncryptionPadding.Pkcs1);
+
+            //we might want a string representation of our cypher text... base64 will do
+            var encryptedText = Convert.ToBase64String(bytesEncryptedText);
+
+            return encryptedText;
+        }
+
+        public static string  RSAPrivateKeyDecrypt(string privatekey, string apiId)
+        {
+            //Console.WriteLine("entered APIID : " + apiId);
+            var bytesEncryptedText = Convert.FromBase64String(apiId);
+
+            //we want to decrypt, therefore we need a csp and load our private key
+            var csp = new RSACryptoServiceProvider(2048);
+            csp.FromXmlString(privatekey);
+
+            //decrypt and strip pkcs#1.5 padding
+            byte[] bytesPlainTextData = csp.Decrypt(bytesEncryptedText, RSAEncryptionPadding.Pkcs1);
+
+            //get our original plainText back...
+            var plainTextData = Encoding.UTF8.GetString(bytesPlainTextData);
+            return plainTextData;
+        }
+        public static void generateOutputFile(IDictionary<string, string> keyPairMap, string outputFilePath)
+        {
+            string filepath = outputFilePath;
+            using (StreamWriter file = new StreamWriter(filepath))
+                foreach (var entry in keyPairMap)
+                    file.WriteLine("[{0} {1}]", entry.Key, entry.Value);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace Aes_Example
+{
+    class AesExample
+    {
+        public static void Main()
+        {
+            IDictionary<string, string> keyPairMap = new Dictionary<string, string>();
+            string outputFilePath = null;
+
+            Console.Write("Please enter the PartnerID : ");
+            string partnerID = Console.ReadLine();
+            keyPairMap.Add("api-id", partnerID);
+
+            Console.Write("Please enter the Request/Response file path : ");
+            string jsonFilePath = Console.ReadLine();
+            string plainText = File.ReadAllText(jsonFilePath);
+
+            Console.Write("Encryption or Decryption , input either E or D : ");
+            string processToFollow = Console.ReadLine();
+
+            Console.Write("Please enter the outputfile path : ");
+            outputFilePath = Console.ReadLine();
+
+            if (processToFollow.Trim() == "E")
+            {
+                Console.Write("Please enter the public key file path: ");
+                string publicKeyxmlpath = Console.ReadLine();
+                string publickey = File.ReadAllText(publicKeyxmlpath);
+
+                using (AesManaged aes = new AesManaged())
+                {
+                    aes.Mode = CipherMode.CBC;
+                    aes.Padding = PaddingMode.PKCS7;
+                    aes.BlockSize = 128;
+                    aes.KeySize = 128;
+                    aes.IV = EncryptionUtil.copyOfRange(aes.Key, 0, 16);
+
+                    // Encrypt string    
+                    byte[] encryptedBytes = EncryptionUtil.AESEncrypt(plainText, aes.Key, aes.IV);
+                    string aesHexKey = BitConverter.ToString(aes.Key).Replace("-", string.Empty);
+                    //Console.WriteLine("AES Hex Key: " + aesHexKey);
+
+                    var hexEncryptedBody = BitConverter.ToString(encryptedBytes).Replace("-", string.Empty);
+                    Debug.WriteLine("Encrypted Data Hex: \n" + hexEncryptedBody);
+                    keyPairMap.Add("EncodedJsonBody", hexEncryptedBody);
+
+                    string RSAEncryptedKey = EncryptionUtil.RSAPublicKeyEncrypt(publickey, aesHexKey, partnerID);
+                    Debug.WriteLine("API Key : \n" + RSAEncryptedKey);
+                    keyPairMap.Add("api-key", RSAEncryptedKey);
+
+                }
+            }
+            else if (processToFollow == "D")
+            {
+                Console.SetIn(new StreamReader(Console.OpenStandardInput(8192)));
+                Console.Write("Please enter the api-key : ");
+                string apiId = Console.ReadLine();
+                keyPairMap.Add("api-key", apiId);
+
+                Console.Write("Please enter the private key file path : ");
+                string privateKeyxmlpath = Console.ReadLine();
+                string privateKey = File.ReadAllText(privateKeyxmlpath);
+
+                string decryptedText =  EncryptionUtil.RSAPrivateKeyDecrypt(privateKey, apiId);
+                string hexKey = decryptedText; //decryptedText.Replace(";;",";").Split(';')[1];
+                Debug.WriteLine("Decrypted Hex AES Key : " + hexKey);
+
+                byte[] byteKey = EncryptionUtil.HexadecimalStringToByteArray(hexKey);
+
+                string decrypted = EncryptionUtil.AESDecrypt(plainText, byteKey, EncryptionUtil.copyOfRange(byteKey, 0, 16));
+                Debug.WriteLine("Decrypted data : {decrypted} " + decrypted);
+                keyPairMap.Add("Decrypted data", decrypted);
+            }
+            EncryptionUtil.generateOutputFile(keyPairMap, outputFilePath);
+            Console.WriteLine("Please check the outputfile");
+            Console.ReadLine();
+        }
+    }
+
+   
+
+}
+
