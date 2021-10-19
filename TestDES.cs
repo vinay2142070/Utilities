41cdf6b01ca05c912ae60ff612c7daf92ad5c630
vinay2142070 <50856679+vinay2142070@users.noreply.github.com>
2021-10-19 19:46:07 +0530

    C# encrypt and decrypt code for TripleDes

--- TestDES.cs
+++ TestDES.cs
@@ -1,4 +1,11 @@
-using Microsoft.VisualBasic;
+/******************************************************************************
+
+                            Online C# Compiler.
+                Code, Compile, Run and Debug C# program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -10,51 +17,99 @@ using System.Text;
 using System.IO;
 
 
-
-public class Test
+class HelloWorld {
+  static void Main() {
+    Console.WriteLine(Decrypt2("l5ducOfM1vQUfA6/K/bf23+GVWAl64i0","false"));
+  }
+  
+  public static void PrintByteArray(byte[] bytes)
 {
-    public static void Main()
+    var sb = new StringBuilder("new byte[] { ");
+    foreach (var b in bytes)
     {
-         try
-        {
-            DESCryptoServiceProvider myDESProvider = new DESCryptoServiceProvider();
-             myDESProvider.Key = ASCIIEncoding.ASCII.GetBytes("12345678");
-            myDESProvider.IV = ASCIIEncoding.ASCII.GetBytes("12345678");
-
-            ICryptoTransform myICryptoTransform = myDESProvider.CreateEncryptor(myDESProvider.Key, myDESProvider.IV);
-
-            FileStream ProcessFileStream = new FileStream("testmd.txt", FileMode.Open, FileAccess.Read);
-            FileStream ResultFileStream = new FileStream("testenc.txt", FileMode.Create, FileAccess.Write);
-            CryptoStream myCryptoStream = new CryptoStream(ResultFileStream, myICryptoTransform, CryptoStreamMode.Write);
-             byte[] bytearrayinput = new byte[ProcessFileStream.Length];
-             ProcessFileStream.Read(bytearrayinput, 0, bytearrayinput.Length);
-            myCryptoStream.Write(bytearrayinput, 0, bytearrayinput.Length);
-             myCryptoStream.Close();
-            ProcessFileStream.Close();
-            ResultFileStream.Close();
-            
-            myDESProvider.IV = ASCIIEncoding.ASCII.GetBytes("12345677");
-             FileStream DecryptedFile = new FileStream("testenc.txt", FileMode.Open, FileAccess.Read);
-
-             myICryptoTransform = myDESProvider.CreateDecryptor(myDESProvider.Key, myDESProvider.IV);
-
-             myCryptoStream = new CryptoStream(DecryptedFile, myICryptoTransform, CryptoStreamMode.Read);
-
-             StreamReader myDecStreamReader = new StreamReader(myCryptoStream);
-
-            StreamWriter myDecStreamWriter = new StreamWriter("testdec.txt");
-
-             myDecStreamWriter.Write(myDecStreamReader.ReadToEnd());
-
-             myCryptoStream.Close();
-
-            myDecStreamReader.Close();
-
-            myDecStreamWriter.Close();
-        }
-        catch (Exception ex)
-        {
-           Console.WriteLine(ex.ToString());
-        }
+        sb.Append(b + ", ");
     }
+    sb.Append("}");
+    Console.WriteLine(sb.ToString());
+}
+   
+  public static string Encrypt2(string clearText,string key)
+{
+    try
+    {
+        string encryptedText = "";
+        MD5 md5 = new MD5CryptoServiceProvider();
+        TripleDES des = new TripleDESCryptoServiceProvider();
+       // des.KeySize = 192;
+        des.Mode = CipherMode.ECB;
+        des.Padding = PaddingMode.PKCS7;
+
+       // byte[] md5Bytes = md5.ComputeHash(Encoding.Unicode.GetBytes(key));
+
+        //byte[] ivBytes = new byte[8];
+
+       byte[] readText = File.ReadAllBytes(@"encryptkey.dat");
+      
+      sbyte[] signed = {-2, 69, -63, -110, -128, -2, -5, 28, 104, 87, -111, 64, 1, -26, -105, 4, -110, -113, -14, 73, -63, -75, -65, -39};
+        byte[] unsigned = (byte[]) (Array)signed;
+        PrintByteArray(readText);
+         Console.WriteLine("length:"+readText.Length);
+         
+        
+      
+        des.Key = unsigned;
+
+      //  des.IV = ivBytes;
+
+        byte[] clearBytes = Encoding.Unicode.GetBytes(clearText);
+
+        ICryptoTransform ct = des.CreateEncryptor();
+        using (MemoryStream ms = new MemoryStream())
+        {
+            using (CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write))
+            {
+                cs.Write(clearBytes, 0, clearBytes.Length);
+                cs.Close();
+            }
+            encryptedText = Convert.ToBase64String(ms.ToArray());
+        }
+        return encryptedText;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
+    }
+    return "";
+}
+
+public static string Decrypt2(string cipher,string key)
+{
+    try
+    {
+        byte[] clearBytes = Convert.FromBase64String(cipher);
+        MD5 md5 = new MD5CryptoServiceProvider();
+        byte[] md5Bytes = md5.ComputeHash(Encoding.Unicode.GetBytes(key));
+        string encryptedText = "";
+        TripleDES des = new TripleDESCryptoServiceProvider();
+       //des.KeySize = 192;
+        des.Mode = CipherMode.ECB;
+        des.Padding = PaddingMode.PKCS7;
+        byte[] ivBytes = new byte[8];
+      byte[] readText = File.ReadAllBytes(@"encryptkey.dat");
+      sbyte[] signed = {-2, 69, -63, -110, -128, -2, -5, 28, 104, 87, -111, 64, 1, -26, -105, 4, -110, -113, -14, 73, -63, -75, -65, -39};
+        byte[] unsigned = (byte[]) (Array)signed;
+       // Console.WriteLine("key size in bytes"+Convert.FromBase64String("AAECAwQFBgcICQoLDA0ODw=="));
+        des.Key = unsigned;
+       // des.IV = ivBytes;
+        ICryptoTransform ct = des.CreateDecryptor();
+        byte[] resultArray = ct.TransformFinalBlock(clearBytes, 0, clearBytes.Length);
+        encryptedText = Encoding.Default.GetString(resultArray);
+        return encryptedText;
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.ToString());
+    }
+     return "";
+}
 }
